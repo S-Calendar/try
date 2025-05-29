156d0591703c17d35d0bcdb4ba4dd4f830a5ba57
IOYX <seoyeon2julie@naver.com>
2025-05-29 20:59:49 +0900

    Update notice.dart

--- lib/models/notice.dart
+++ lib/models/notice.dart
@@ -1,3 +1,4 @@
+// notice.dart
 import 'package:flutter/material.dart';
 
 class Notice {
@@ -6,18 +7,31 @@ class Notice {
   final DateTime endDate;
   final Color color;
 
+  /// 관심 공지 여부
+  bool isFavorite;
+
   Notice({
     required this.title,
     required this.startDate,
     required this.endDate,
     required this.color,
+    this.isFavorite = false,
   });
 
   // 신청기간 길이 (띠 우선순위 판단용)
   int get duration => endDate.difference(startDate).inDays + 1;
 
-  // 해당 날짜가 포함되는지 여부
+  // 날짜 포함 여부
   bool includes(DateTime date) {
     return !date.isBefore(startDate) && !date.isAfter(endDate);
   }
+
+  @override
+  bool operator ==(Object other) =>
+      other is Notice &&
+      title == other.title &&
+      startDate == other.startDate;
+
+  @override
+  int get hashCode => Object.hash(title, startDate);
 }
