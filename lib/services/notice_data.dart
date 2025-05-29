5205967b9f8b24e9dfcf62797a3d4065bb0f237d
haseokyung6 <153154159+haseokyung6@users.noreply.github.com>
2025-05-29 18:35:53 +0700

    Update notice_data.dart

--- lib/services/notice_data.dart
+++ lib/services/notice_data.dart
@@ -6,7 +6,7 @@ import '../models/notice.dart';
 class NoticeData {
   static Future<List<Notice>> loadNoticesFromJson(BuildContext context) async {
     final String jsonString = await rootBundle.loadString(
-      'assets/임시2_output.json',
+      'assets/임시2_추가_output.json',
     );
     final Map<String, dynamic> jsonMap = json.decode(jsonString);
 
@@ -15,7 +15,7 @@ class NoticeData {
     jsonMap.forEach((category, notices) {
       for (var item in notices) {
         final title = item['제목'] ?? '제목 없음';
-        final term = item['신청기간'] ?? item['결과발표일(하는날)'] ?? '';
+        final term = item['신청기간'] ?? item['결과발표일(하는날)'] ?? item['결과발표일'] ?? '';
         final dates = term.split('~').map((s) => s.trim()).toList();
 
         if (dates.isEmpty || dates.first.isEmpty) continue;
@@ -27,20 +27,22 @@ class NoticeData {
                   ? DateTime.parse(dates[1].replaceAll('.', '-'))
                   : start;
 
+          Color color;
+          if (category == '학사공지') {
+            color = const Color(0x83ABC9FF); // 50% 투명도
+          } else if (category == 'ai학과공지') {
+            color = const Color(0x83FFABAB);
+          } else if (category == '취업공지') {
+            color = const Color(0x83A5FAA5);
+          } else {
+            color = const Color.fromARGB(131, 171, 200, 255); // 기본 fallback
+          }
+
           all.add(
-            Notice(
-              title: title,
-              startDate: start,
-              endDate: end,
-              color:
-                  category == 'ai학과공지'
-                      ? const Color(0xFFFFABAB)
-                      : const Color(0xFFABC9FF),
-            ),
+            Notice(title: title, startDate: start, endDate: end, color: color),
           );
         } catch (_) {
-          // 날짜 파싱 실패 → 건너뜀
-          continue;
+          continue; // 날짜 파싱 실패 → 건너뜀
         }
       }
     });
