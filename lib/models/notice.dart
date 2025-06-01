c00727f8c479b213955f663e07c164eeeae7951e
haseokyung6 <153154159+haseokyung6@users.noreply.github.com>
2025-06-01 17:39:18 +0700

    url 필드 추가

--- lib/models/notice.dart
+++ lib/models/notice.dart
@@ -6,32 +6,21 @@ class Notice {
   final DateTime startDate;
   final DateTime endDate;
   final Color color;
-
-  /// 관심 공지 여부
-  bool isFavorite;
+  final String? url;
 
   Notice({
     required this.title,
     required this.startDate,
     required this.endDate,
     required this.color,
-    this.isFavorite = false,
+    this.url,
   });
 
   // 신청기간 길이 (띠 우선순위 판단용)
   int get duration => endDate.difference(startDate).inDays + 1;
 
-  // 날짜 포함 여부
+  // 해당 날짜가 포함되는지 여부
   bool includes(DateTime date) {
     return !date.isBefore(startDate) && !date.isAfter(endDate);
   }
-
-  @override
-  bool operator ==(Object other) =>
-      other is Notice &&
-      title == other.title &&
-      startDate == other.startDate;
-
-  @override
-  int get hashCode => Object.hash(title, startDate);
 }
